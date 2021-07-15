0773a6d9c6c0e0951927e2d89a5269485f8af3bb
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-14 23:22:21 -0500

    Set variables for current time, on or off, and set time

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,7 +1,11 @@
 
 class Alarm_Clock:
     def __init__(self, curTime, onOrOff, setTime):
-        self.alarm_set = False
+        self.cutTime = curTime
+        self.onOrOff = onOrOff
+        self.setTime = setTime
+
+    #Method to set the current time    
 
     def switchAlarmOnOff(self):
         if (self.alarm_set == False):
